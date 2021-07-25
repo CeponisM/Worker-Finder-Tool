7548d99c32e0a6fb3998b3fa9cabd3c4bd3e28b2
Mantas C <mantasceponis5@gmail.com>
2021-07-25 00:00:18 -0500

    Directory conficuration
    Modified the configuration files for Apache and MySQL to reflect the GitHub directory

---
+++ htdocs/test/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="submit.php" method="post">
+	Username:<br>
+	<input type="text" name="username" >
+	<br>
+	Password:<br>
+	<input type="text" name="password">
+	<br><br>
+	<input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
