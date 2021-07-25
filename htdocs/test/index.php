b18f19a01024d399eb2228b3c1e0b8edb0d436c4
Mantas C <mantasceponis5@gmail.com>
2021-07-25 13:22:43 -0500

    Index header, and scrolling menu bar
    Worked on the index to add a header, and a menu bar that scrolls with the page.

--- htdocs/test/index.php
+++ htdocs/test/index.php
@@ -1,12 +1,204 @@
 <!DOCTYPE html>
 <html>
-<body>
-
 <head>
-<div>
-	<p align="right"><button type="button" onclick="window.location.href='/test/SignIn.php';">Sign In!</button></p>
-</div>
+	<link rel="stylesheet" href="styles.css">
+	<title> WFT - Home</title>
 </head>
+
+<body>
+<div class="top-container">
+  <h1>Worker Finder Tool</h1>
+  <p></p>
+</div>
+
+<div class="header" id="myHeader">
+	<div class="menuMain">
+	</div>
+	<div class="menuButtons">
+		<button type="button" onclick="window.location.href='/test/SignIn.php';">Sign In</button>
+		<button type="button" onclick="window.location.href='/test/Account.php';">Account</button>
+	</div>
+	<br style="clear:both;"/>
+</div>
+test 1
+<br>
+test 2
+<br>
+test 3
+<br>
+test 4
+<br>
+test 5
+<br>
+test 6
+<br>
+test 7
+<br>
+test 8
+<br>
+test 9
+<br>
+test 10
+<br>
+test 11
+<br>
+test 12
+<br>
+test 13
+<br>
+test 14
+<br>
+test 15
+<br>
+test 16
+<br>
+test 17
+<br>
+test 18
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+
+<br>
+<br>
+
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+
+<br>
+<br>
+
+
+<br>
+<br>
+
+
+<br>
+<br>
+
+<br>
+<br>
+
+
+<br>
+<br>
+
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+
+
+<br>
+<br>
+
+
+<br>
+<br>
+
+
+<br>
+<br>
+
+<br>
+<br>
+
+<br>
+<br>
+h
+
+<script>
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+</script>
 
 </body>
 </html>
